1210a752659d934da0c6cb5a3a1343aac1beb9f6
MichelBallen <maballen83@soy.sena.edu.co>
2024-03-22 13:04:18 -0500

    .

--- src/main/java/org/parking/java/entities/Cliente.java
+++ src/main/java/org/parking/java/entities/Cliente.java
@@ -4,52 +4,38 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Cliente {
-    public String nombre;
+    public String nombres;
     public String apellidos;
     public Long numeroIdentificacion;
     public List<Vehiculo> misVehiculos = new ArrayList<Vehiculo>();
+    
 
-
-public Cliente() {
+    
+    public Cliente() {
     }
 
-
-public Cliente(String nombre, 
-               String apellidos, 
-               Long numeroIdentificacion) {
-    this.nombre = nombre;
-    this.apellidos = apellidos;
-    this.numeroIdentificacion = numeroIdentificacion;
-}
-
-
-
-
-public Cliente(String nombre, 
-               String apellidos, 
-               Long numeroIdentificacion, 
-               List<Vehiculo> misVehiculos) {
-        this.nombre = nombre;
+    public Cliente(String nombres, String apellidos, Long numeroIdentificacion) {
+        this.nombres = nombres;
         this.apellidos = apellidos;
         this.numeroIdentificacion = numeroIdentificacion;
-        this.misVehiculos = misVehiculos;
     }
-public void addVehicle (Vehiculo vehiculo) {
-    //anadir el carrito a la lista
-    //this: para acceder a atributos 
-    //y metodos al interior del objeto
-    //NOTA: usenla al interior de metodos 
-    this.misVehiculos.add(vehiculo);
-}
-//Otra sobrecarga del metodo 
-//addVehicle
-public void addVehicle(String placa,
-                    TipoVehiculo tv) {
-            Vehiculo v = new Vehiculo();
-            v.placa = placa ;
-            v.tipoVehiculo = tv ;
-    this.misVehiculos.add(v);
 
-                    }
+    //Definir metodo de la clase
+    //addCar: vinvular un carrito al cliente
+    public void addVehicle(Vehiculo vehiculo){
 
+        //añadir el carrito a la lista
+        //this:para acceder a atributos
+        //y metodos al interior del objeto
+        //NOTA:usenla al interior de metodos
+        this.misVehiculos.add(vehiculo);
+    }
+    
+    //otra sobrecarga del metodo addvehivle
+    public void addVehicle(String placa,TipoVehiculo tv){
+        Vehiculo v = new Vehiculo();
+        v.placa = placa;
+        v.tipoVehiculo = tv;
+        this.misVehiculos.add(v);
+    }
 }
--- src/main/java/org/parking/java/entities/Vehiculo.java
+++ src/main/java/org/parking/java/entities/Vehiculo.java
@@ -1,26 +1,25 @@
 package org.parking.java.entities;
 
 public class Vehiculo {
+
     //atributos de clase
-    //1. modificador de acceso
-    //2. tipo de dato del atributo
-    //3. nombre atributo
-public String placa;
-public TipoVehiculo tipoVehiculo;
+    //1.modificador de acceso
+    //2.tipo de dato del atributo
+    //3.nombre del atributo
+    public String placa;
+    public TipoVehiculo tipoVehiculo;
 
-//constructor
-//Sin parametros: constructor por defecto
-public Vehiculo() {
-}
+    //constructor
+    //sin parametros: constructor por defecto
+    public Vehiculo() {
+    }
 
-//constructor paramerizado
-public Vehiculo(String placa,
-               TipoVehiculo tipoVehiculo) {
-    this.placa = placa;
-    this.tipoVehiculo = tipoVehiculo;
-}
+    //constructor parametrizado
+    public Vehiculo(String placa, TipoVehiculo tipoVehiculo) {
+        this.placa = placa;
+        this.tipoVehiculo = tipoVehiculo;
+    }
 
-
-
-
+    
+    
 }
